6ff4b0712026761cd8e50be8e4cee979d9eec50c
Your Name <uwasandrine99@gmail.com>
2023-04-13 19:24:51 +0200

    file changed

---
+++ index.js
@@ -0,0 +1,29 @@
+// var c=0;
+// function pop() {
+//    if(c==0)
+//    {
+//    getComputedStyle.ElementByTagName("button").style.display="block"
+//    }
+//   }
+// console.log(document.links)
+// let button=document.querySelector("button");
+// let body=document.querySelector("#body");
+// if(button){
+//     button.addEventListener("click",() =>{ 
+//         body.classList.toggle("open")
+//      })
+//  }
+    // (document).readyState(function(){
+    //     ('button').click(function(){
+    //         ('#body'.toggleClass('#body'))
+    //     })
+    // })
+// var button = document.getElementsById("body");
+// button.addEventListener('click',body)
+// function button(body){
+//    console.log(e.target.value)
+// }
+function button(){
+    document.getElementById("body").style.display="flex"
+    // window.location = "#body"
+     }
